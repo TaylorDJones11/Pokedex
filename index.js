6989388669ff17b449a356813e77129935d8b1b9
Taylor J <72883059+TaylorDJones11@users.noreply.github.com>
2024-06-24 14:34:56 -0500

    calling API

--- index.js
+++ index.js
@@ -14,11 +14,34 @@ const getPokemon = async (id) => {
     //api call
     const url = `https://pokeapi.co/api/v2/pokemon/${id}/`;
     const res = await fetch(url);
-    const poke = await res.json();
-    console.log(poke);
+    const pokemon = await res.json();
+    createPokemonCard(pokemon);
   } catch (error) {
     console.log(error);
   }
+};
+
+const createPokemonCard = (pokemon) => {
+  const pokemonEl = document.createElement('div');
+  pokemonEl.classList.add('pokemon');
+
+  const pokemonInnerHTML = `<div class='img-container'>
+     <img
+      src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png'
+       alt=''
+     />
+    </div>
+   <div class='info'>
+     <span class='number'>${pokemon.id}</span>
+      <h3 class='name'>${pokemon.name}</h3>
+     <small class='type'>
+      
+       Type: <span></span>${pokemon.types[0].type.name}
+      </small>
+    </div>`;
+
+  pokemonEl.innerHTML = pokemonInnerHTML;
+  pokeContainer.appendChild(pokemonEl);
 };
 
 fetchPokemon();
